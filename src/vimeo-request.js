6f66c453b4745466e205e2f4122b17c26ae55ac6
Matt Olsen <olsen_matthew@yahoo.com>
2016-11-19 14:37:41 -0800

    styling video content

--- src/vimeo-request.js
+++ src/vimeo-request.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react'
-import AppHeader from './App-Header.js'
 import BurgerMenu from './burger-menu.js'
 import ContactBar from './sm-contact-bar.js'
+import { Link } from 'react-router'
+import Radium from 'radium'
+
+let RadiumLink = Radium(Link)
 
 class VimeoRequest extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      peace: []
+      peace: [],
+      data: []
     }
     this.onData = this.onData.bind(this) //muy importante
   }
   onData ( data ) {
     const parsedVimeoData = JSON.parse(data.currentTarget.response).data
+    console.log(parsedVimeoData[0].name,'<----parsedVimeoData');
     console.log(parsedVimeoData[0].uri.slice(8),'<----parsedVimeoData');
-    this.setState({peace: parsedVimeoData.map((el, index)=>{
+    this.setState({
+      peace: parsedVimeoData.map((el, index)=>{
               return (`https://player.vimeo.com/video/${el.uri.slice(8)}?badge=0&portrait=0&byline=0`)
-            })})
+      }),
+      data: parsedVimeoData
+    })
   }
   componentDidMount () {
     let oReq = new XMLHttpRequest()
@@ -38,14 +46,25 @@ class VimeoRequest extends Component {
         <BurgerMenu />
         <main id="page-wrap">
           <ContactBar />
-          <AppHeader />
+          <div className="App-header">
+            <RadiumLink className="header-link" to="/">
+              <h2 className="page-header fade-in one" >HVM | Work</h2>
+            </RadiumLink>
+          </div>
         </main>
         <div className="videos-container">
           <div className="videos-wrapper">
-            {this.state.peace.map((el, index)=>{
+            {this.state.data.map((el, index)=>{
+              console.log(el)
               return (
-                <div className="iframe-wrap">
-                  <iframe className="frame-style" key={index} src={el} frameBorder='0' allowFullScreen></iframe>
+                <div className="content-wrap" key={index}>
+                  <div className="iframe-wrap">
+                    <iframe className="frame-style" src={`https://player.vimeo.com/video/${el.uri.slice(8)}?badge=0&portrait=0&byline=0`} frameBorder='0' allowFullScreen></iframe>
+                  </div>
+                  <div className="title-n-desc">
+                    <h3>{el.name}</h3>
+                    <p>{el.description}</p>
+                  </div>
                 </div>
               )
             })}
